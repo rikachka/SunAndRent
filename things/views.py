c6e2e7386b8ba92322df9394cca88c2e4ae2f99c
rikachka <rikachka1@gmail.com>
2017-11-25 20:35:48 +0200

    Logged in user

--- things/views.py
+++ things/views.py
@@ -5,6 +5,9 @@ from .models import Good, Customer, StoragedGood, Reservation
 from django.urls import reverse
 
 
+CUSTOMER_INDEX = 1
+
+
 def catalogues(request):
     template = loader.get_template('things/catalogues.html')
     context = {}
@@ -15,16 +18,14 @@ def catalogue_stored(request):
     # good = Good.objects.get(pk=1)
     # good.status = '0'
     # good.save()
-    index = 1
-    owner = Customer.objects.get(pk=index)
+    owner = Customer.objects.get(pk=CUSTOMER_INDEX)
     items = StoragedGood.objects.filter(good_id__status='0').filter(good_id__owner_id=owner)
     template = loader.get_template('things/catalogue_stored.html')
     return HttpResponse(template.render({'stored_items' : items}, request))
 
 
 def catalogue_rented(request):
-    index = 2
-    customer = Customer.objects.get(pk=index)
+    customer = Customer.objects.get(pk=CUSTOMER_INDEX)
     items = Reservation.objects.filter(customer_id=customer)
     template = loader.get_template('things/catalogue_rented.html')
     return HttpResponse(template.render({'rented_items' : items}, request))
