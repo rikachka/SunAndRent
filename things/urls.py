b0ae22402588ed48fd2b98ea04806261719291eb
izeren <yushalnik@bk.ru>
2017-11-25 19:28:29 +0300

    Add all goods catalogue

--- things/urls.py
+++ things/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^catalogues$', views.catalogues, name='catalogues'),
+    url(r'^catalogue_goods$', views.catalogue_goods, name='catalogue_goods'),
     url(r'^catalogue_stored$', views.catalogue_stored, name='catalogue_stored'),
     url(r'^catalogue_rented$', views.catalogue_rented, name='catalogue_rented'),
-    url(r'^index2$', views.index2, name='index2'),
 ]
