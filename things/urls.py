a3414f5e1af06662bbb3a6508c865ca85d79a307
rikachka <rikachka1@gmail.com>
2017-11-25 14:07:03 +0200

    Added catalogue

--- things/urls.py
+++ things/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index$', views.index, name='index'),
+    url(r'^catalogues$', views.catalogues, name='catalogues'),
+    url(r'^catalogue_stored$', views.catalogue_stored, name='catalogue_stored'),
     url(r'^index2$', views.index2, name='index2'),
 ]
--- things/views.py
+++ things/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def index(request):
-    template = loader.get_template('things/index.html')
+def catalogues(request):
+    template = loader.get_template('things/catalogues.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def catalogue_stored(request):
+    template = loader.get_template('things/catalogue_stored.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
