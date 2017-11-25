3da2b81237c2f95b326d9e314519cd1a7760f6fa
izeren <yushalnik@bk.ru>
2017-11-25 23:32:46 +0300

    Add search per differents tabs

--- things/urls.py
+++ things/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     url(r'^catalogue_stored$', views.catalogue_stored, name='catalogue_stored'),
     url(r'^catalogue_rented$', views.catalogue_rented, name='catalogue_rented'),
     url(r'^return_good_to_owner/(?P<item_pk>[0-9]+)$', views.return_good_to_owner, name='return_good_to_owner'),
+    url(r'^search_rented$', views.search_items_rented, name='search_rented'),
+    url(r'^search_stored$', views.search_items_stored, name='search_stored'),
+    url(r'^search_goods$', views.search_items_goods, name='search_goods'),
     url(r'^item_stored', views.item_stored_fullinfo, name='item_stored_fullinfo'),
 ]
