8b249b2fa59c9111aba528ed5b389d64d02bc572
rikachka <rikachka1@gmail.com>
2017-11-25 13:53:15 +0200

    Added main page

--- things/views.py
+++ things/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the things index.")
+    template = loader.get_template('things/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def index2(request):
+    return HttpResponse("Hello, world. You're at the index2.")
