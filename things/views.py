bafd8fee8df6c7921ea255b2e292272ade83180c
rikachka <rikachka1@gmail.com>
2017-11-25 16:40:32 +0200

    Base info about goods

--- things/views.py
+++ things/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Good
 
 def catalogues(request):
     template = loader.get_template('things/catalogues.html')
@@ -8,9 +9,9 @@ def catalogues(request):
     return HttpResponse(template.render(context, request))
 
 def catalogue_stored(request):
+    items = Good.objects.all()
     template = loader.get_template('things/catalogue_stored.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render({'items' : items}, request))
 
 def index2(request):
     return HttpResponse("Hello, world. You're at the index2.")
